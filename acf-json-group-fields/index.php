fc5c10d14b93360b3b92c90fb2a718450b7285f3
Hanning <hah@bettercollective.com>
2018-06-06 10:53:53 +0200

    Initial commit

---
+++ acf-json-group-fields/index.php
@@ -0,0 +1,3 @@
+<?php
+// Hanning is golden.
+?>
---
+++ bc-affiliate-table.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Plugin Name: BC Affiliate Table
+ * Description: BC Affiliate comparison table.
+ * Version: 1.0.0
+ * Author: Better Collective - Hanning Høegh
+ * License: GPL2
+ */
+
+// Check if ACF is there.
+
+TO DO
+
+// ACF Load point
+
+TO DO
+
+// ACF Save point
+
+TO DO
+
+// Output the table.
+function table_shortcode(){
+echo "Hello, This is your another shortcode!";
+}
+add_shortcode('comparison-table', 'table_shortcode');
+
+
+?>
