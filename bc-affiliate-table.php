d9cfc43746b0d64d35d2908772998f369c3308cd
Hanning <hah@bettercollective.com>
2018-10-04 17:04:42 +0200

    CSS List fixes

--- bc-affiliate-table.php
+++ bc-affiliate-table.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: BC Affiliate Tables
  * Description: BC Affiliate comparison tables.
- * Version: 1.2.0
+ * Version: 1.2.1
  * Author: Better Collective - Hanning Høegh
  * License: GPL2
  */
