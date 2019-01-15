b7bd6725e88382042bee64fed54899354703c40d
SmileyJoey <hah@bettercollective.com>
2019-01-15 11:12:04 +0100

    Changed empty checks to PHP function equivalent.

--- view/shortcode-table.php
+++ view/shortcode-table.php
@@ -173,17 +173,17 @@ if ( !function_exists( 'at_output_table' ) ) {
 					<!-- ticks -->
 					<div class="col-tick">
 						<a href="<?php the_sub_field('cta_affiliate_link'); ?>" onclick="" rel="nofollow" target="_blank" class="tick-link">
-                            <?php if( !get_sub_field('first_pointer_visible') == '' ): ?>
+                            <?php if( !empty(get_sub_field('first_pointer_visible'))): ?>
 							<span class="tick-item">
 								<svg class="icon" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path d="M23.334 11.96c-.713-.726-.872-1.829-.393-2.727.342-.64.366-1.401.064-2.062-.301-.66-.893-1.142-1.601-1.302-.991-.225-1.722-1.067-1.803-2.081-.059-.723-.451-1.378-1.062-1.77-.609-.393-1.367-.478-2.05-.229-.956.347-2.026.032-2.642-.776-.44-.576-1.124-.915-1.85-.915-.725 0-1.409.339-1.849.915-.613.809-1.683 1.124-2.639.777-.682-.248-1.44-.163-2.05.229-.61.392-1.003 1.047-1.061 1.77-.082 1.014-.812 1.857-1.803 2.081-.708.16-1.3.642-1.601 1.302s-.277 1.422.065 2.061c.479.897.32 2.001-.392 2.727-.509.517-.747 1.242-.644 1.96s.536 1.347 1.17 1.7c.888.495 1.352 1.51 1.144 2.505-.147.71.044 1.448.519 1.996.476.549 1.18.844 1.902.798 1.016-.063 1.953.54 2.317 1.489.259.678.82 1.195 1.517 1.399.695.204 1.447.072 2.031-.357.819-.603 1.936-.603 2.754 0 .584.43 1.336.562 2.031.357.697-.204 1.258-.722 1.518-1.399.363-.949 1.301-1.553 2.316-1.489.724.046 1.427-.249 1.902-.798.475-.548.667-1.286.519-1.996-.207-.995.256-2.01 1.145-2.505.633-.354 1.065-.982 1.169-1.7s-.135-1.443-.643-1.96zm-12.584 5.43l-4.5-4.364 1.857-1.857 2.643 2.506 5.643-5.784 1.857 1.857-7.5 7.642z"/></svg><?php the_sub_field('first_pointer_visible'); ?>
 							</span>
                             <?php endif; ?>
-                            <?php if( !get_sub_field('second_pointer_visible') == '' ): ?>
+                            <?php if( !empty(get_sub_field('second_pointer_visible'))): ?>
 							<span class="tick-item">
 								<svg class="icon" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path d="M23.334 11.96c-.713-.726-.872-1.829-.393-2.727.342-.64.366-1.401.064-2.062-.301-.66-.893-1.142-1.601-1.302-.991-.225-1.722-1.067-1.803-2.081-.059-.723-.451-1.378-1.062-1.77-.609-.393-1.367-.478-2.05-.229-.956.347-2.026.032-2.642-.776-.44-.576-1.124-.915-1.85-.915-.725 0-1.409.339-1.849.915-.613.809-1.683 1.124-2.639.777-.682-.248-1.44-.163-2.05.229-.61.392-1.003 1.047-1.061 1.77-.082 1.014-.812 1.857-1.803 2.081-.708.16-1.3.642-1.601 1.302s-.277 1.422.065 2.061c.479.897.32 2.001-.392 2.727-.509.517-.747 1.242-.644 1.96s.536 1.347 1.17 1.7c.888.495 1.352 1.51 1.144 2.505-.147.71.044 1.448.519 1.996.476.549 1.18.844 1.902.798 1.016-.063 1.953.54 2.317 1.489.259.678.82 1.195 1.517 1.399.695.204 1.447.072 2.031-.357.819-.603 1.936-.603 2.754 0 .584.43 1.336.562 2.031.357.697-.204 1.258-.722 1.518-1.399.363-.949 1.301-1.553 2.316-1.489.724.046 1.427-.249 1.902-.798.475-.548.667-1.286.519-1.996-.207-.995.256-2.01 1.145-2.505.633-.354 1.065-.982 1.169-1.7s-.135-1.443-.643-1.96zm-12.584 5.43l-4.5-4.364 1.857-1.857 2.643 2.506 5.643-5.784 1.857 1.857-7.5 7.642z"/></svg><?php the_sub_field('second_pointer_visible'); ?>
 							</span>
                             <?php endif; ?>
-                            <?php if( !get_sub_field('third_pointer_visible') == '' ): ?>
+                            <?php if( !empty(get_sub_field('third_pointer_visible'))): ?>
 							<span class="tick-item">
 								<svg class="icon" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path d="M23.334 11.96c-.713-.726-.872-1.829-.393-2.727.342-.64.366-1.401.064-2.062-.301-.66-.893-1.142-1.601-1.302-.991-.225-1.722-1.067-1.803-2.081-.059-.723-.451-1.378-1.062-1.77-.609-.393-1.367-.478-2.05-.229-.956.347-2.026.032-2.642-.776-.44-.576-1.124-.915-1.85-.915-.725 0-1.409.339-1.849.915-.613.809-1.683 1.124-2.639.777-.682-.248-1.44-.163-2.05.229-.61.392-1.003 1.047-1.061 1.77-.082 1.014-.812 1.857-1.803 2.081-.708.16-1.3.642-1.601 1.302s-.277 1.422.065 2.061c.479.897.32 2.001-.392 2.727-.509.517-.747 1.242-.644 1.96s.536 1.347 1.17 1.7c.888.495 1.352 1.51 1.144 2.505-.147.71.044 1.448.519 1.996.476.549 1.18.844 1.902.798 1.016-.063 1.953.54 2.317 1.489.259.678.82 1.195 1.517 1.399.695.204 1.447.072 2.031-.357.819-.603 1.936-.603 2.754 0 .584.43 1.336.562 2.031.357.697-.204 1.258-.722 1.518-1.399.363-.949 1.301-1.553 2.316-1.489.724.046 1.427-.249 1.902-.798.475-.548.667-1.286.519-1.996-.207-.995.256-2.01 1.145-2.505.633-.354 1.065-.982 1.169-1.7s-.135-1.443-.643-1.96zm-12.584 5.43l-4.5-4.364 1.857-1.857 2.643 2.506 5.643-5.784 1.857 1.857-7.5 7.642z"/></svg><?php the_sub_field('third_pointer_visible'); ?>
 							</span>
@@ -195,7 +195,7 @@ if ( !function_exists( 'at_output_table' ) ) {
 					<div class="col-btn">
 						<a href="<?php the_sub_field('cta_affiliate_link'); ?>" onclick="" rel="nofollow" target="_blank" role="button" class="at-btn at-btn-success">
 							<strong>
-				                <?php if( !$sign_up_text == '' ): ?>
+				                <?php if( !empty($sign_up_text)): ?>
                                     <span class="hidden-sm-up"><?php echo $sign_up_text; ?> + </span>
 				                <?php endif; ?>
                             <?php the_sub_field('cta_button_text'); ?>
@@ -217,7 +217,7 @@ if ( !function_exists( 'at_output_table' ) ) {
                             </div>
                         <?php } ?>
 
-                        <?php if( get_sub_field('include_a_tc_popup_box_on_hover') == 'yes' && !get_sub_field('tc_popup_content' == '') ): ?>
+                        <?php if( get_sub_field('include_a_tc_popup_box_on_hover') == 'yes' && !empty(get_sub_field('tc_popup_content'))): ?>
 						<!-- Popup content that shows on mobile devices -->
 						<div class="extra-text">
 							<?php the_sub_field('tc_popup_content'); ?>
@@ -244,17 +244,17 @@ if ( !function_exists( 'at_output_table' ) ) {
 								</li>
 							</ul> -->
 							<ul style="margin-top: 1em;" class="list-icons hidden-md-down">
-                                <?php if( !get_sub_field('first_pointer') == '' ): ?>
+                                <?php if( !empty(get_sub_field('first_pointer'))): ?>
 								<li>
 							        <svg class="icon" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path d="M20.285 2l-11.285 11.567-5.286-5.011-3.714 3.716 9 8.728 15-15.285z"/></svg><?php the_sub_field('first_pointer'); ?>
 							    </li>
                                 <?php endif; ?>
-                                <?php if( !get_sub_field('second_pointer') == '' ): ?>
+                                <?php if( !empty(get_sub_field('second_pointer'))): ?>
 							    <li>
 							        <svg class="icon" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path d="M20.285 2l-11.285 11.567-5.286-5.011-3.714 3.716 9 8.728 15-15.285z"/></svg><?php the_sub_field('second_pointer'); ?>
 							    </li>
                                 <?php endif; ?>
-                                <?php if( !get_sub_field('third_pointer') == '' ): ?>
+                                <?php if( !empty(get_sub_field('third_pointer'))): ?>
 							    <li>
 							        <svg class="icon" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path d="M20.285 2l-11.285 11.567-5.286-5.011-3.714 3.716 9 8.728 15-15.285z"/></svg><?php the_sub_field('third_pointer'); ?>
 							    </li>
@@ -311,7 +311,7 @@ if ( !function_exists( 'at_output_table' ) ) {
 
 
 						<ul class="list-icons hidden-lg-up">
-                            <?php if( !get_sub_field('first_pointer') == '' ): ?>
+                            <?php if( !empty(get_sub_field('first_pointer'))): ?>
 							<li>
 								<svg class="icon icon-tick" width="1em" height="1em">
 								<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="<?php echo AT_URL ?>build/images/svg-symbols.svg#tick" width="100%" height="100%"></use>
@@ -319,7 +319,7 @@ if ( !function_exists( 'at_output_table' ) ) {
 								<?php the_sub_field('first_pointer'); ?>
 							</li>
                             <?php endif; ?>
-                            <?php if( !get_sub_field('second_pointer') == '' ): ?>
+                            <?php if( !empty(get_sub_field('second_pointer'))): ?>
 							<li>
 								<svg class="icon icon-tick" width="1em" height="1em">
 								<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="<?php echo AT_URL ?>build/images/svg-symbols.svg#tick" width="100%" height="100%"></use>
@@ -327,7 +327,7 @@ if ( !function_exists( 'at_output_table' ) ) {
 								<?php the_sub_field('second_pointer'); ?>
 							</li>
                             <?php endif; ?>
-                            <?php if( !get_sub_field('third_pointer') == '' ): ?>
+                            <?php if( !empty(get_sub_field('third_pointer'))): ?>
 							<li>
 								<svg class="icon icon-tick" width="1em" height="1em">
 								<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="<?php echo AT_URL ?>build/images/svg-symbols.svg#tick" width="100%" height="100%"></use>
